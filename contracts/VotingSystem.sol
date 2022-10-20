a3da0bb957cb2f440df50b90feed25f59e7caebd
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-10-20 22:02:58 +0200

    Voting System contract

---
+++ contracts/VotingSystem.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract VotingSystem {
+  enum Choice {
+    VERY_UNSATISFACTORY,
+    UNSATISFACTORY,
+    PASSABLE,
+    SATISFACTORY,
+    VERY_SATISFACTORY
+  }
+
+  struct Proposal {
+    string content;
+    uint score;
+  }
+
+  struct VotingSession {
+    string description;
+    bool isOpen;
+  }
+
+  event VotingSessionCreated(uint votingSessionId, address chairman);
+  event VoteRegistered(uint votingSessionId, address voter);
+  event VotingSessionClosed(uint votingSessionId);
+
+  modifier onlyWhenExists(uint _votingSessionId) {
+    require(_votingSessionId < _votingSessions.length, "Voting session not found");
+    _;
+  }
+
+  modifier onlyWhenOpen(uint _votingSessionId) {
+    require(_votingSessionId < _votingSessions.length, "Voting session not found");
+    require(_votingSessions[_votingSessionId].isOpen, "This voting session is already closed.");
+    _;
+  }
+
+  VotingSession[] private _votingSessions;
+  mapping(uint => address) private _votingSessionToChairman;
+  mapping(uint => Proposal[]) private _proposalsBySession;
+  mapping(uint => mapping(address => bool)) private _votersBySession;
+
+  function initVotingSession(
+    string memory _description, 
+    string[] memory _proposalContents
+  ) external {
+    require(bytes(_description).length > 0, "You must provide a description for the voting session.");
+
+    VotingSession memory session = VotingSession({
+      description: _description,
+      isOpen: true
+    });
+
+    _votingSessions.push(session);
+    uint votingSessionId = _votingSessions.length - 1;
+     _votingSessionToChairman[votingSessionId] = msg.sender;
+
+    for(uint i = 0; i < _proposalContents.length; i++) {
+      _proposalsBySession[votingSessionId].push(Proposal({
+        content: _proposalContents[i],
+        score: 0
+      }));
+    }
+
+    emit VotingSessionCreated(votingSessionId, msg.sender);
+  }
+
+  function getVotingSessions(uint _from, uint _to) external view returns (VotingSession[] memory) {
+    require(_from < _to && _to <= _votingSessions.length);
+
+    VotingSession[] memory votingSessions = new VotingSession[](_to - _from);
+    for (uint i = _from; i < _to; i++) {
+      votingSessions[i] = _votingSessions[i];
+    }
+    return votingSessions;
+  }
+
+  function getProposalContents(uint _votingSessionId) external view onlyWhenExists(_votingSessionId) returns (string[] memory) {
+    Proposal[] memory currentProposals = _proposalsBySession[_votingSessionId];
+    string[] memory contents = new string[](currentProposals.length);
+
+    for (uint i = 0; i < currentProposals.length; i++) {
+      contents[i] = currentProposals[i].content;
+    }
+    return contents;
+  }
+
+  function vote(uint _votingSessionId, Choice[] memory _choices) external onlyWhenOpen(_votingSessionId) {
+    require(!_votersBySession[_votingSessionId][msg.sender]);
+    require(_choices.length == _proposalsBySession[_votingSessionId].length);
+
+    _votersBySession[_votingSessionId][msg.sender] = true;
+    for (uint i = 0; i < _choices.length; i++) {
+      _proposalsBySession[_votingSessionId][i].score += uint(_choices[i]);
+    }
+
+    emit VoteRegistered(_votingSessionId, msg.sender);
+  }
+
+  function close(uint _votingSessionId) external onlyWhenOpen(_votingSessionId) {
+    require(_votingSessionToChairman[_votingSessionId] == msg.sender);
+
+    _votingSessions[_votingSessionId].isOpen = false;
+
+    emit VotingSessionClosed(_votingSessionId);
+  }
+}
