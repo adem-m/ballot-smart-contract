1f8d564557ae9f7f21454297b9ea828f69dbb0c2
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-10-28 23:29:11 +0200

    added fuji network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import { config as envConfig } from "dotenv";
+envConfig();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { get } from "env-var";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [get("FUJI_ACCOUNT").required().asString()]
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const VotingSystem = await ethers.getContractFactory("VotingSystem");
+  const votingSystem = await VotingSystem.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  await votingSystem.deployed();
+  console.log(`Voting system deployed`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
