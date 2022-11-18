6d9172dba2c36d63b40c11590d99c37da3fd4d6b
Adem Mrizak <adem.mrizak@gmail.com>
2022-11-18 18:19:28 +0100

    Add winning proposal

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -2,100 +2,113 @@
 pragma solidity ^0.8.9;
 
 contract VotingSystem {
-  enum Choice {
-    VERY_UNSATISFACTORY,
-    UNSATISFACTORY,
-    PASSABLE,
-    SATISFACTORY,
-    VERY_SATISFACTORY
-  }
-
-  struct Proposal {
-    string content;
-    uint score;
-  }
-
-  struct VotingSession {
-    string description;
-    bool isOpen;
-  }
-
-  event VotingSessionCreated(uint votingSessionId, address chairman);
-  event VoteRegistered(uint votingSessionId, address voter);
-  event VotingSessionClosed(uint votingSessionId);
-
-  modifier onlyWhenExists(uint _votingSessionId) {
-    require(_votingSessionId < votingSessions.length, "Voting session not found.");
-    _;
-  }
-
-  modifier onlyWhenOpen(uint _votingSessionId) {
-    require(_votingSessionId < votingSessions.length, "Voting session not found.");
-    require(votingSessions[_votingSessionId].isOpen, "This voting session is already closed.");
-    _;
-  }
-
-  VotingSession[] public votingSessions;
-  mapping(uint => address) private _votingSessionToChairman;
-  mapping(uint => Proposal[]) private _proposalsBySession;
-  mapping(uint => mapping(address => bool)) private _votersBySession;
-
-  function initVotingSession(
-    string memory _description, 
-    string[] memory _proposalContents
-  ) external {
-    require(bytes(_description).length > 0, "You must provide a description for the voting session.");
-
-    VotingSession memory session = VotingSession({
-      description: _description,
-      isOpen: true
-    });
-
-    votingSessions.push(session);
-    uint votingSessionId = votingSessions.length - 1;
-     _votingSessionToChairman[votingSessionId] = msg.sender;
-
-    for(uint i = 0; i < _proposalContents.length; i++) {
-      _proposalsBySession[votingSessionId].push(Proposal({
-        content: _proposalContents[i],
-        score: 0
-      }));
+    enum Choice {
+        VERY_UNSATISFACTORY,
+        UNSATISFACTORY,
+        PASSABLE,
+        SATISFACTORY,
+        VERY_SATISFACTORY
     }
 
-    emit VotingSessionCreated(votingSessionId, msg.sender);
-  }
-
-  function getVotingSessions(uint _from, uint _to) external view returns (VotingSession[] memory) {
-    require(_from < _to && _to <= votingSessions.length);
-
-    VotingSession[] memory sessions = new VotingSession[](_to - _from);
-    for (uint i = _from; i < _to; i++) {
-      sessions[i] = votingSessions[i];
-    }
-    return sessions;
-  }
-
-  function getProposals(uint _votingSessionId) external view onlyWhenExists(_votingSessionId) returns (Proposal[] memory) {
-    return _proposalsBySession[_votingSessionId];
-  }
-
-  function vote(uint _votingSessionId, Choice[] memory _choices) external onlyWhenOpen(_votingSessionId) {
-    require(!_votersBySession[_votingSessionId][msg.sender], "You have already voted.");
-    require(_choices.length == _proposalsBySession[_votingSessionId].length, "Incorrect number of choices.");
-
-    _votersBySession[_votingSessionId][msg.sender] = true;
-    for (uint i = 0; i < _choices.length; i++) {
-      _proposalsBySession[_votingSessionId][i].score += uint(_choices[i]);
+    struct Proposal {
+        string content;
+        uint decidingVote;
     }
 
-    emit VoteRegistered(_votingSessionId, msg.sender);
-  }
+    struct VotingSession {
+        string description;
+        bool isOpen;
+    }
 
-  function close(uint _votingSessionId) external onlyWhenOpen(_votingSessionId) {
-    require(_votingSessionToChairman[_votingSessionId] == msg.sender, "You are not the chairman.");
+    event VotingSessionCreated(uint votingSessionId, address chairman);
+    event VoteRegistered(uint votingSessionId, address voter);
+    event VotingSessionClosed(uint votingSessionId);
 
-    votingSessions[_votingSessionId].isOpen = false;
+    modifier onlyWhenExists(uint _votingSessionId) {
+        require(_votingSessionId < votingSessions.length, "Voting session not found.");
+        _;
+    }
 
-    emit VotingSessionClosed(_votingSessionId);
-  }
+    modifier onlyWhenOpen(uint _votingSessionId) {
+        require(_votingSessionId < votingSessions.length, "Voting session not found.");
+        require(votingSessions[_votingSessionId].isOpen, "This voting session is already closed.");
+        _;
+    }
+
+    VotingSession[] public votingSessions;
+    mapping(uint => address) private _votingSessionToChairman;
+    mapping(uint => Proposal[]) private _proposalsBySession;
+    mapping(uint => mapping(address => bool)) private _votersBySession;
+
+    function initVotingSession(
+        string memory _description,
+        string[] memory _proposalContents
+    ) external {
+        require(bytes(_description).length > 0, "You must provide a description for the voting session.");
+
+        VotingSession memory session = VotingSession({
+        description : _description,
+        isOpen : true
+        });
+
+        votingSessions.push(session);
+        uint votingSessionId = votingSessions.length - 1;
+        _votingSessionToChairman[votingSessionId] = msg.sender;
+
+        for (uint i = 0; i < _proposalContents.length; i++) {
+            _proposalsBySession[votingSessionId].push(Proposal({
+            content : _proposalContents[i],
+            decidingVote : 0
+            }));
+        }
+
+        emit VotingSessionCreated(votingSessionId, msg.sender);
+    }
+
+    function getVotingSessions(uint _from, uint _to) external view returns (VotingSession[] memory) {
+        require(_from < _to && _to <= votingSessions.length);
+
+        VotingSession[] memory sessions = new VotingSession[](_to - _from);
+        for (uint i = _from; i < _to; i++) {
+            sessions[i] = votingSessions[i];
+        }
+        return sessions;
+    }
+
+    function getProposals(uint _votingSessionId) external view onlyWhenExists(_votingSessionId) returns (Proposal[] memory) {
+        return _proposalsBySession[_votingSessionId];
+    }
+
+    function vote(uint _votingSessionId, Choice[] memory _choices) external onlyWhenOpen(_votingSessionId) {
+        require(!_votersBySession[_votingSessionId][msg.sender], "You have already voted.");
+        require(_choices.length == _proposalsBySession[_votingSessionId].length, "Incorrect number of choices.");
+
+        _votersBySession[_votingSessionId][msg.sender] = true;
+        for (uint i = 0; i < _choices.length; i++) {
+            if (_choices[i] == Choice.SATISFACTORY || _choices[i] == Choice.VERY_SATISFACTORY) {
+                _proposalsBySession[_votingSessionId][i].decidingVote++;
+            }
+        }
+
+        emit VoteRegistered(_votingSessionId, msg.sender);
+    }
+
+    function close(uint _votingSessionId) external onlyWhenOpen(_votingSessionId) {
+        require(_votingSessionToChairman[_votingSessionId] == msg.sender, "You are not the chairman.");
+
+        votingSessions[_votingSessionId].isOpen = false;
+
+        emit VotingSessionClosed(_votingSessionId);
+    }
+
+    function getWinningProposal(uint _votingSessionId) external view onlyWhenExists(_votingSessionId) returns (Proposal memory) {
+        require(!votingSessions[_votingSessionId].isOpen, "This voting session is still open.");
+        Proposal memory winner = _proposalsBySession[_votingSessionId][0];
+        for (uint i = 1; i < _proposalsBySession[_votingSessionId].length; i++) {
+            if (_proposalsBySession[_votingSessionId][i].decidingVote > winner.decidingVote) {
+                winner = _proposalsBySession[_votingSessionId][i];
+            }
+        }
+        return winner;
+    }
 }
--- test/VotingSystem.test.ts
+++ test/VotingSystem.test.ts
@@ -1,160 +1,172 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
+import {expect} from "chai";
+import {ethers} from "hardhat";
 
 
 describe("Voting system", () => {
-  const votingSessions = [
-    { description: "A tale of three idiots", proposals: ["Paul", "Paolo", "Adem"] },
-    { description: "Best chain ?", proposals: ["Ethereum", "Binance", "Polygon", "Avalanche", "Solana"] }
-  ];
+    const votingSessions = [
+        {description: "A tale of three idiots", proposals: ["Paul", "Paolo", "Adem"]},
+        {description: "Best chain ?", proposals: ["Ethereum", "Binance", "Polygon", "Avalanche", "Solana"]}
+    ];
 
-  async function deployContractFixture() {
-    // Contracts are deployed using the first signer/account by default
-    const [owner, account1, account2] = await ethers.getSigners();
+    async function deployContractFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, account1, account2] = await ethers.getSigners();
 
-    const VotingSystem = await ethers.getContractFactory("VotingSystem");
-    const contract = await VotingSystem.deploy();
+        const VotingSystem = await ethers.getContractFactory("VotingSystem");
+        const contract = await VotingSystem.deploy();
 
-    return { contract, owner, account1, account2 };
-  }
+        return {contract, owner, account1, account2};
+    }
 
-  async function createVotingSessionWithAccount1() {
-    const { contract, owner, account1, account2 } = await deployContractFixture();
-    await contract.connect(account1).initVotingSession(
-      votingSessions[0].description,
-      votingSessions[0].proposals
-    );
+    async function createVotingSessionWithAccount1() {
+        const {contract, owner, account1, account2} = await deployContractFixture();
+        await contract.connect(account1).initVotingSession(
+            votingSessions[0].description,
+            votingSessions[0].proposals
+        );
 
-    return { contract, owner, account1, account2 };
-  }
+        return {contract, owner, account1, account2};
+    }
 
-  describe("initVotingSession", () => {
-    it("should create a new voting session", async () => {
-      const { contract, account1 } = await loadFixture(deployContractFixture);
-      const _contract = contract.connect(account1);
+    describe("initVotingSession", () => {
+        it("should create a new voting session", async () => {
+            const {contract, account1} = await loadFixture(deployContractFixture);
+            const _contract = contract.connect(account1);
 
-      await expect(contract.votingSessions(0)).to.be.reverted;
-      await expect(_contract.initVotingSession(
-        votingSessions[0].description,
-        votingSessions[0].proposals
-      )).to.be.fulfilled;
-      await expect(contract.votingSessions(0)).to.be.fulfilled;
+            await expect(contract.votingSessions(0)).to.be.reverted;
+            await expect(_contract.initVotingSession(
+                votingSessions[0].description,
+                votingSessions[0].proposals
+            )).to.be.fulfilled;
+            await expect(contract.votingSessions(0)).to.be.fulfilled;
+        });
+
+        it("should emit a 'VotingSessionCreated' event", async () => {
+            const {contract, account1} = await loadFixture(deployContractFixture);
+            const _contract = contract.connect(account1);
+            await expect(_contract.initVotingSession(
+                votingSessions[0].description,
+                votingSessions[0].proposals
+            )).to
+                .emit(_contract, "VotingSessionCreated")
+                .withArgs(0, account1.address);
+        });
+
+        it("should revert when the description is empty", async () => {
+            const {contract, account1} = await loadFixture(deployContractFixture);
+            const _contract = contract.connect(account1);
+            await expect(_contract.initVotingSession(
+                "",
+                votingSessions[0].proposals
+            )).to.be.revertedWith("You must provide a description for the voting session.");
+        });
     });
 
-    it("should emit a 'VotingSessionCreated' event", async () => {
-      const { contract, account1 } = await loadFixture(deployContractFixture);
-      const _contract = contract.connect(account1);
-      await expect(_contract.initVotingSession(
-        votingSessions[0].description,
-        votingSessions[0].proposals
-      )).to
-        .emit(_contract, "VotingSessionCreated")
-        .withArgs(0, account1.address);
+    describe("vote", () => {
+        it("should update the scores of each proposal", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
+
+            expect((await contract.getProposals(0)).map(p => p.decidingVote)).to.deep.equal([0, 0, 0]);
+            await expect(contract.connect(account2).vote(0, [1, 2, 3])).to.be.fulfilled;
+            expect((await contract.getProposals(0)).map(p => p.decidingVote)).to.deep.equal([0, 0, 1]);
+        });
+
+        it("should emit a 'VoteRegistered' event", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+
+            await expect(_contract.vote(0, [1, 2, 3]))
+                .to
+                .emit(_contract, "VoteRegistered")
+                .withArgs(0, account2.address);
+        });
+
+        it("should revert when the voting session does not exist", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+
+            await expect(_contract.vote(3, [1, 2, 3])).to.be.revertedWith("Voting session not found.");
+        });
+
+        it("should revert when voting more than once", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+            await _contract.vote(0, [1, 2, 3]);
+
+            await expect(_contract.vote(0, [1, 2, 3])).to.be.revertedWith("You have already voted.");
+        });
+
+        it("should revert when the voting session is already closed", async () => {
+            const {contract, account1, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+            await contract.connect(account1).close(0);
+
+            await expect(_contract.vote(0, [1, 2, 3])).to.be.revertedWith("This voting session is already closed.");
+        });
+
+        it("should revert when providing too few choices", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+
+            await expect(_contract.vote(0, [1, 2])).to.be.revertedWith("Incorrect number of choices.");
+        });
+
+        it("should revert when providing too many choices", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+
+            await expect(_contract.vote(0, [1, 2, 3, 4])).to.be.revertedWith("Incorrect number of choices.");
+        });
     });
 
-    it("should revert when the description is empty", async () => {
-      const { contract, account1 } = await loadFixture(deployContractFixture);
-      const _contract = contract.connect(account1);
-      await expect(_contract.initVotingSession(
-        "",
-        votingSessions[0].proposals
-      )).to.be.revertedWith("You must provide a description for the voting session.");
-    });
-  });
+    describe("close", () => {
+        it("should close the voting session", async () => {
+            const {contract, account1} = await loadFixture(createVotingSessionWithAccount1);
 
-  describe("vote", () => {
-    it("should update the scores of each proposal", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
+            await expect(contract.connect(account1).close(0)).to.be.fulfilled;
+        });
 
-      expect((await contract.getProposals(0)).map(p => p.score)).to.deep.equal([0, 0, 0]);
-      await expect(contract.connect(account2).vote(0, [1, 2, 3])).to.be.fulfilled;
-      expect((await contract.getProposals(0)).map(p => p.score)).to.deep.equal([1, 2, 3]);
-    });
+        it("should emit a 'VotingSesionClosed' event", async () => {
+            const {contract, account1} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account1);
 
-    it("should emit a 'VoteRegistered' event", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account2);
+            await expect(_contract.close(0))
+                .to
+                .emit(_contract, "VotingSessionClosed")
+                .withArgs(0);
+        });
 
-      await expect(_contract.vote(0, [1, 2, 3]))
-        .to
-        .emit(_contract, "VoteRegistered")
-        .withArgs(0, account2.address);
-    });
+        it("should revert when the voting session does not exist", async () => {
+            const {contract, account1} = await loadFixture(createVotingSessionWithAccount1);
 
-    it("should revert when the voting session does not exist", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account2);
+            await expect(contract.connect(account1).close(2)).to.be.revertedWith("Voting session not found.");
+        });
 
-      await expect(_contract.vote(3, [1, 2, 3])).to.be.revertedWith("Voting session not found.");
-    });
+        it("should revert when the the caller is not the chairman", async () => {
+            const {contract, account2} = await loadFixture(createVotingSessionWithAccount1);
 
-    it("should revert when voting more than once", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account2);
-      await _contract.vote(0, [1, 2, 3]);
+            await expect(contract.connect(account2).close(0)).to.be.revertedWith("You are not the chairman.");
+        });
 
-      await expect(_contract.vote(0, [1, 2, 3])).to.be.revertedWith("You have already voted.");
+        it("should revert when the voting session is already closed", async () => {
+            const {contract, account1} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account1);
+            await _contract.close(0);
+
+            await expect(_contract.close(0)).to.be.revertedWith("This voting session is already closed.");
+        });
     });
 
-    it("should revert when the voting session is already closed", async () => {
-      const { contract, account1, account2 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account2);
-      await contract.connect(account1).close(0);
+    describe("winner", () => {
+        it("should return the winning proposal", async () => {
+            const {contract, account1, account2} = await loadFixture(createVotingSessionWithAccount1);
+            const _contract = contract.connect(account2);
+            await _contract.vote(0, [1, 2, 3]);
+            await contract.connect(account1).close(0);
+            const winningProposal = await contract.getWinningProposal(0);
 
-      await expect(_contract.vote(0, [1, 2, 3])).to.be.revertedWith("This voting session is already closed.");
+            expect(winningProposal.decidingVote).to.equal(1);
+        });
     });
-
-    it("should revert when providing too few choices", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account2);
-
-      await expect(_contract.vote(0, [1, 2])).to.be.revertedWith("Incorrect number of choices.");
-    });
-
-    it("should revert when providing too many choices", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account2);
-
-      await expect(_contract.vote(0, [1, 2, 3, 4])).to.be.revertedWith("Incorrect number of choices.");
-    });
-  });
-
-  describe("close", () => {
-    it("should close the voting session", async () => {
-      const { contract, account1 } = await loadFixture(createVotingSessionWithAccount1);
-
-      await expect(contract.connect(account1).close(0)).to.be.fulfilled;
-    });
-
-    it("should emit a 'VotingSesionClosed' event", async () => {
-      const { contract, account1 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account1);
-
-      await expect(_contract.close(0))
-        .to
-        .emit(_contract, "VotingSessionClosed")
-        .withArgs(0);
-    });
-
-    it("should revert when the voting session does not exist", async () => {
-      const { contract, account1 } = await loadFixture(createVotingSessionWithAccount1);
-
-      await expect(contract.connect(account1).close(2)).to.be.revertedWith("Voting session not found.");
-    });
-
-    it("should revert when the the caller is not the chairman", async () => {
-      const { contract, account2 } = await loadFixture(createVotingSessionWithAccount1);
-
-      await expect(contract.connect(account2).close(0)).to.be.revertedWith("You are not the chairman.");
-    });
-
-    it("should revert when the voting session is already closed", async () => {
-      const { contract, account1 } = await loadFixture(createVotingSessionWithAccount1);
-      const _contract = contract.connect(account1);
-      await _contract.close(0);
-
-      await expect(_contract.close(0)).to.be.revertedWith("This voting session is already closed.");
-    });
-  });
 });
